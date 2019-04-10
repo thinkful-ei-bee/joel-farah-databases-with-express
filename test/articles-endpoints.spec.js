bedcec33bf1949266c603a1fcdce1406fddaeb15
joelcorey <joelcorey@fastmail.com>
2019-04-10 09:43:13 -0700

    add tests

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -9,38 +9,69 @@ describe.only('Articles Endpoints', function() {
     before('make knex instance', () => {
       db = knex({
         client: 'pg',
-        connection: 'process.env.TEST_DB_URL',
+        connection: process.env.TEST_DB_URL,
       })
       app.set('db', db)
     })
-
   
     after('disconnect from db', () => db.destroy())
   
     before('clean the table', () => db('blogful_articles').truncate())
 
-    afterEach('cleanup', ()  => db('blogful_articles').truncate())
-  
-    context('Given there are articles in the database', () => {
-        const testArticles = makeArticlesArray();
+  afterEach('cleanup', () => db('blogful_articles').truncate())
 
-        beforeEach('insert articles', () => {
-        return db
-            .into('blogful_articles')
-            .insert(testArticles)
-        })
-
-        it('GET /articles responds with 200 and all of the articles', () => {
-            return supertest(app)
-            .get('/articles')
-            .expect(200, testArticles)
-        })
-        it('GET /article/:article_id responds with 200 and the specified article', () => {
-            const articleId = 2
-            const expectedArticle = testArticles[articleId - 1]
-            return supertest(app)
-            .get(`/articles/${articleId}`)
-            .expect(200, expectedArticle)
-        })
+  describe(`GET /articles`, () => {
+    context(`Given no articles`, () => {
+      it(`responds with 200 and an empty list`, () => {
+        return supertest(app)
+          .get('/articles')
+          .expect(200, [])
+      })
     })
+    
+    context('Given there are articles in the database', () => {
+      const testArticles = makeArticlesArray()
+
+      beforeEach('insert articles', () => {
+        return db
+          .into('blogful_articles')
+          .insert(testArticles)
+      })
+
+      it('responds with 200 and all of the articles', () => {
+        return supertest(app)
+          .get('/articles')
+          .expect(200, testArticles)
+      })
+    })
+  })
+
+  describe(`GET /articles/:article_id`, () => {
+    context(`Given no articles`, () => {
+      it(`responds with 404`, () => {
+        const articleId = 123456
+        return supertest(app)
+          .get(`/articles/${articleId}`)
+          .expect(404, { error: { message: `Article doesn't exist` } })
+      })
+    })
+    
+    context('Given there are articles in the database', () => {
+      const testArticles = makeArticlesArray()
+
+      beforeEach('insert articles', () => {
+        return db
+          .into('blogful_articles')
+          .insert(testArticles)
+      })
+
+      it('responds with 200 and the specified article', () => {
+        const articleId = 2
+        const expectedArticle = testArticles[articleId - 1]
+        return supertest(app)
+          .get(`/articles/${articleId}`)
+          .expect(200, expectedArticle)
+      })
+    })
+  })
 })
