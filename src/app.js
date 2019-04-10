1383beafc4d84cf14c463d2ddd67505915c24dd8
Farah Student <farah.gus@hotmail.com>
2019-04-10 11:51:03 -0400

    get article by id

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const ArticlesService = require('./articles-service')
 
 const app = express()
 
@@ -13,6 +14,23 @@ const morganOption = (process.env.NODE_ENV === 'production')
 
 app.use(cors())
 app.use(helmet())
+
+app.get('/articles', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  ArticlesService.getAllArticles(knexInstance)
+    .then(articles => {
+      res.json(articles)
+    })
+    .catch(next)
+})
+
+app.get('/articles/:article_id', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  ArticlesService.getById(knexInstance, req.params.article_id)
+    .then(article => {
+      res.json(article)
+    })
+})
 
 app.get('/', (req, res) => {
   res.send('Hello, boilerplate!')
